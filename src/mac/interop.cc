da1ba4c0df3ae7b627e1f16f71f63395afae4b06
Antonis Kalou <kalouantonis@gmail.com>
2016-01-08 23:32:19 +0100

    Removed fancy-pancy description parser from cfTypeToCString

--- src/mac/interop.cc
+++ src/mac/interop.cc
@@ -23,41 +23,10 @@ char *cfTypeToCString(CFTypeRef cfString)
     return "";
   }
 
-  // Check that we're actually using a string
-  if(CFGetTypeID(cfString) != CFStringGetTypeID()) {
-    // Attempt to convert it to a string
-    CFStringRef stringRef = CFStringCreateWithFormat(NULL, NULL, CFSTR("%@"), cfString);
+  // Attempt to convert it to a string
+  CFStringRef stringRef = CFStringCreateWithFormat(NULL, NULL, CFSTR("%@"), cfString);
 
-    return cfStringRefToCString(stringRef);
-  }
-
-  static char deviceFilePath[2048];
-
-  deviceFilePath[0] = '\0';
-
-  CFStringGetCString(CFCopyDescription(cfString),
-                     deviceFilePath,
-                     MAXPATHLEN,
-                     kCFStringEncodingASCII);
-
-  char* p = deviceFilePath;
-
-  while (*p != '\"')
-    p++;
-
-  p++;
-
-  char* pp = p;
-
-  while (*pp != '\"')
-    pp++;
-
-  *pp = '\0';
-
-  if (isdigit(*p))
-    *p = 'x';
-
-  return p;
+  return cfStringRefToCString(stringRef);
 }
 
 int cfTypeToInteger(CFTypeRef cfNumber)
